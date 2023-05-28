2dc360d0ef7ca20e4d335202cbef5afbd68dd39d
Tasleem <tasleembca31@gmail.com>
2023-05-28 16:16:13 +0530

    modified express app

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -10,7 +10,7 @@ export default async (app: Application) => {
     app.use(express.static(path.join(__dirname, 'uploads')));
 
     app.get('/ping', (req: Request, res: Response) => {
-        return res.send('pong 🏓')
+        return res.send('ping pong')
     })
 
     app.use('/admin', AdminRoute);
