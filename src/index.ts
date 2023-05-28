dd682070dcc5e93aa07178f42147a22c970da845
Tasleem <tasleembca31@gmail.com>
2023-05-28 14:57:44 +0530

    feature added

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
-import express from 'express';
-import App from './services/ExpressApp';
-import dbConnection from './services/Database';
-import { PORT } from './config';
+// import express from 'express';
+// import App from './services/ExpressApp';
+// import dbConnection from './services/Database';
+// import { PORT } from './config';
 
-const startServer = async () => {
-    const app = express();
+// const startServer = async () => {
+//     const app = express();
 
-    await dbConnection();
-    await App(app);
+//     await dbConnection();
+//     await App(app);
 
-    app.listen(PORT, () => {
-        console.log(`Listening to port ${PORT}`);
-    })
-}
+//     app.listen(PORT, () => {
+//         console.log(`Listening to port ${PORT}`);
+//     })
+// }
 
-startServer();
+// startServer();
 
-// import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 
-// const app = express()
-// const port = process.env.PORT || 8080
+const app = express()
+const port = process.env.PORT || 8080
 
-// app.get('/', (_req: Request, res: Response) => {
-//     return res.send('Express Typescript on Vercel')
-// })
+app.get('/', (_req: Request, res: Response) => {
+    return res.send('Express Typescript on Vercel')
+})
 
-// app.get('/ping', (_req: Request, res: Response) => {
-//     return res.send('pong 🏓')
-// })
+app.get('/ping', (_req: Request, res: Response) => {
+    return res.send('pong 🏓')
+})
 
-// app.listen(port, () => {
-//     return console.log(`Server is listening on ${port}`)
-// })
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`)
+})
